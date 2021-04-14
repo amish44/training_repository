8a44c3723ae575b0efd964a862c1c8d8b9a49cce
amish44 <amish.choudhary@coupa.com>
2021-04-14 14:00:42 +0530

    Updated code

--- spec/feature/basic_core_test/trainx_spec.rb
+++ spec/feature/basic_core_test/trainx_spec.rb
@@ -1,7 +1,7 @@
 require 'capybara/rspec'
 require 'webdrivers'
 
-feature 'TrainX tests', type: :feature do
+feature 'TrainX specs', type: :feature do
 	
 	Capybara.app_host = 'https://dashmaster-r30-analytics.coupadev.com'
 	Capybara.default_driver = :selenium_chrome
@@ -9,14 +9,16 @@ feature 'TrainX tests', type: :feature do
 	scenario 'Visit trainx' do
 		visit '/'
 		fill_in 'user_login', with: 'trainxuser'
-		fill_in 'user_password', with: '1234'
+		fill_in 'user_password', with: 'Temp@1234'
 		find('#login_button').send_keys :enter 
 		expect(page.title).to have_content('Coupa Home')
-		click_link('trainx')
+		find('#my_account').hover
 		click_link('Sign Out')
+		Capybara.current_session.driver.quit
 	end
 
 	scenario 'scenario2' do
 		visit 'https://google.com'
+		Capybara.current_session.driver.quit
 	end
 end
